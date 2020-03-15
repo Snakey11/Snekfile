944cf3d9e322f3be3cf4f1f99a9891a0237287f5
Snakey11 <snakey1loa@gmail.com>
2020-03-15 17:42:30 -0400

    Initial Commit

---
+++ MAKEHACK.cmd
@@ -0,0 +1,32 @@
+
+@echo off
+setlocal enabledelayedexpansion
+
+set INPUT=%~dp0Clean.gba
+set OUTPUT=%~dp0New.gba
+set OUTPUT_UPS=%~dp0New.ups
+set MASTER=%~dp0Master.event
+rem You can change these filenames to whatever you like. Don't change the "%~dp0", though!
+
+echo ---------------------------
+
+cd %~dp0
+
+rem Copy our clean ROM to overwrite our edited ROM, so EA has a clean ROM to work with.
+copy "%INPUT%" "%OUTPUT%"
+
+rem Move our working directory to EA's folder.
+cd "Event Assembler"
+
+echo ---------------------------
+echo Assembling ROM. Please wait...
+echo ---------------------------
+
+rem Run Event Assembler on our copy of FE8 clean with our buildfile master.
+Core A FE8 "-output:%OUTPUT%" "-input:%MASTER%"
+
+rem Autogenerate a ups patch of our newly assembled ROM.
+cd "%~dp0ups"
+ups diff -b "%INPUT%" -m "%OUTPUT%" -o "%OUTPUT_UPS%"
+
+pause
